2e9b75bd8ffc021156435ee1cebb96e80f04e63a
Paul Justine Faustino <pauljustinefaustino@Pauls-MacBook-Air.local>
2026-08-22 02:41:58 +0800

    Fix: force HTTPS for APP_URD in endpoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,9 @@ set -e
 # Write environment variables to .env file
 printenv | grep -E "^(APP_|DB_|MAIL_|CACHE_|QUEUE_|SESSION_|FILESYSTEM_)" > .env
 
+# Force APP_URL to use HTTPS
+sed -i 's|^APP_URL=http://|APP_URL=https://|' .env
+
 # Run migrations and start server
 php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=8000
